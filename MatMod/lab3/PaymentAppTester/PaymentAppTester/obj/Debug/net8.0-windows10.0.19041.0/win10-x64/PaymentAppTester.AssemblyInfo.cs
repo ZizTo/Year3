b425cf043aa26646785c69323eb4dad669ee5003
ZizTo <92594525+ZizTo@users.noreply.github.com>
2025-12-01 03:05:17 +0300

    KC

--- MatMod/lab3/PaymentAppTester/PaymentAppTester/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/PaymentAppTester.AssemblyInfo.cs
+++ MatMod/lab3/PaymentAppTester/PaymentAppTester/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/PaymentAppTester.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAppTester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0200713db3a5997c4f7223f1e3f98fae34810495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9246ad700823fb655411009580a02170a2dd4504")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAppTester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAppTester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
